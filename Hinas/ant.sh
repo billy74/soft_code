80099e0decec5af8af1d8e784354e82f3db813df
暴力猪 <billy74@qq.com>
2024-11-28 16:19:25 +0800

    Update ant.sh

--- Hinas/ant.sh
+++ Hinas/ant.sh
@@ -10,7 +10,8 @@ status=$(systemctl is-active --quiet $SERVICE_NAME; echo $?)
 # if [ $# -ne 1 ]; then
    #echo "请使用:bash $0 设备名 进行使用"
    ip_wz=`curl -s -4 ping0.cc/geo | awk 'NR==2'`
-   echo "未使用设备名，将以 ${ip_wz// /} 命名"
+   ip_wz=${ip_wz// /}
+   echo "未使用设备名，将以 $ip_wz 命名"
 # else
 
 INSTALL_PATH="/etc/zhinan"
