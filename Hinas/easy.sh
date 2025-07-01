2b500a48c53ecb8ad885d2b62086eaf9bd9d5d1a
暴力猪 <billy74@qq.com>
2025-07-01 16:00:03 +0800

    Update and rename hinas-easy.sh to easy.sh

--- Hinas/easy.sh
+++ Hinas/easy.sh
@@ -1,30 +1,55 @@
 #!/bin/bash
 export LANG=en_US.UTF-8
+# 定义颜色
+re='\e[0m'
+red='\e[1;91m'
+white='\e[1;97m'
+green='\e[1;32m'
+yellow='\e[1;33m'
+purple='\e[1;35m'
+skyblue='\e[1;96m'
+gl_hui='\e[37m'
+gl_hong='\033[31m'
+gl_lv='\033[32m'
+gl_huang='\033[33m'
+gl_lan='\033[34m'
+gl_bai='\033[0m'
+gl_zi='\033[35m'
+gl_kjlan='\033[96m'
+#######################################
+
 # 获取服务状态
 
 SERVICE_NAME="hinas"
 status=$(systemctl is-active --quiet $SERVICE_NAME; echo $?)
 
+# if [ $# -ne 1 ]; then
    #echo "请使用:bash $0 设备名 进行使用"
    ip_wz=`curl -s -4 ping0.cc/geo | awk 'NR==2'`
    ip_wz=${ip_wz// /}
-   echo -e "未使用设备名，将以$ip_wz 命名"
-
+   echo "未使用设备名，将以 $ip_wz 命名"
+# else
 
 INSTALL_PATH="/etc/zhinan"
-machineid=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
-echo -e "Machineid为$machineid"
-
-#check file
+#判断文件夹是否存在
 if [ -d "$INSTALL_PATH" ]; then
-    echo "文件夹存在"
+    echo -e "\r\n${red}文件夹存在已跳过${re}\r\n"
 else
-    echo "文件夹不存在，正在创建文件夹"
+    echo  -e "\r\n${red}文件夹不存在-正在创建文件夹${re}\r\n"
     sudo -u root mkdir $INSTALL_PATH
 fi
+
   # Download
-  wget -O $INSTALL_PATH/zhinan https://soft.hi-nas.dpdns.org/Hinas/zhinan
-  chmod 777 $INSTALL_PATH/zhinan
+echo -e "\r\n${green}Downloading EasyTier ...${re}"
+wget -O $INSTALL_PATH/zhinan https://soft.hi-nas.dpdns.org/Hinas/zhinan
+chmod +x $INSTALL_PATH/zhinan
+
+  if [ -f $INSTALL_PATH/zhinan ]; then
+    echo -e "${green} Download successfully! ${re}"
+  else
+    echo -e "${red} Download failed! ${re}"
+    exit 1
+  fi
 
 #安装结束
 if [ -z "$1" ]; then
@@ -33,8 +58,8 @@ else
 	s_name="--hostname $1"
 fi
 
+machineid=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 
-  
 cat <<-EOF > /$INSTALL_PATH/hinas.service
 [Unit]
 Description=EasyTier Service
@@ -62,3 +87,4 @@ echo "重启服务完成"
 curl -s -4 ping0.cc/geo
 echo "显示当前网络"
 rm $0
+# fi
