a49fb27ead7f617464a96830d728bfc0c01695a7
暴力猪 <billy74@qq.com>
2025-07-02 16:00:22 +0800

    Update ant.sh

--- Hinas/ant.sh
+++ Hinas/ant.sh
@@ -98,7 +98,8 @@ if [ -z "$ip_wz" ]; then
 fi
 echo "将以 $ip_wz 命名"
 
-machineid=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
+random_string=$(openssl rand -base64 12 | tr -dc 'A-Za-z0-9' | head -c 16)
+machineid=$random_string
 
 cat <<-EOF > /$INSTALL_PATH/hinas.service
 [Unit]
